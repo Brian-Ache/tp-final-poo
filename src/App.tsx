4aaf4b03caec63aaf3367af66a4f1be5ceb50dee
Brian <brianache99@gmail.com>
2025-04-21 17:30:07 -0300

    Terminando visual lista de usuarios del ADM

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import LoginPage from './pages/login/login';
 import RegisterPage from './pages/administrador/register';
-
+import ListaUsuarios from './pages/administrador/listaUsuarios';
+import MainAdm from './pages/administrador/mainAdm';
 
 function App() {
   return (
     <>
-      {/* <LoginPage /> */}
-      <RegisterPage />
-      
+      <MainAdm/>
     </>
   )
 }
--- src/pages/administrador/headerAdm.jsx
+++ src/pages/administrador/headerAdm.jsx
@@ -42,8 +42,8 @@ export default function HeaderAdm() {
                     href={item.href}
                     aria-current={item.current ? 'page' : undefined}
                     className={classNames(
-                      item.current ? 'bg-indigo-600 text-white' : 'text-gray-500 hover:bg-gray-500 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium',
+                      item.current ? 'bg-indigo-600 text-white hover:bg-indigo-500' : 'text-gray-500 hover:bg-gray-500 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium ',
                     )}
                   >
                     {item.name}
@@ -66,7 +66,7 @@ export default function HeaderAdm() {
               aria-current={item.current ? 'page' : undefined}
               className={classNames(
                 item.current ? 'bg-indigo-600 text-white' : 'text-gray-500 hover:bg-gray-500 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium',
+                'block rounded-md px-3 py-2 text-base font-medium ',
               )}
             >
               {item.name}
